0e0a64ca05af39380c6803ec2635de7280a2419a
Dulat Bulat <bbdulat@gmail.com>
2021-04-28 20:30:27 +0600

    Add swift package

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "MobileAnalyticsChartSwift",
+  platforms: [.iOS(.v11)],
+  products: [
+    .library(
+      name: "MobileAnalyticsChartSwift",
+      targets: ["MobileAnalyticsChartSwift"]
+    ),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "MobileAnalyticsChartSwift",
+      dependencies: [],
+      path: "MobileAnalyticsChartSwift"
+    )
+  ]
+)
